3167d2a2183da509d0938e6a1906e1518e06cb7e
Malik Alleyne-Jones <malik.alleynejones@fayette.kyschools.us>
2021-05-13 00:56:39 -0400

    Add xmlns for raw SVGs; update tests accordingly

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -13,7 +13,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('iconic-star')->toHtml();
 
         $expected = <<<'SVG'
-        <svg fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
         SVG;
 
         $this->assertSame($expected, $result);
@@ -25,7 +25,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('iconic-star', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -37,7 +37,7 @@ class CompilesIconsTest extends TestCase
         $result = svg('iconic-star', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<'SVG'
-        <svg style="color: #555" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
+        <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
         SVG;
 
         $this->assertSame($expected, $result);

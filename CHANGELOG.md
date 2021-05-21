f7b1c6b5049558389bc90a6b74d1dcf85453e816
Malik Alleyne-Jones <malik.alleynejones@fayette.kyschools.us>
2021-05-21 18:02:39 -0400

    Add 20 New Icons, Update tests, update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to Blade Iconic will be documented in this file.
 
+## 1.1.0 - 2021-05-13
+
+- Add 20 new icons
+- Add additional tests
 
 ## 1.0.0 - 2021-05-13
 
--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -3,6 +3,7 @@
 namespace ItsMalikJones\BladeIconic\Tests;
 
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Illuminate\Support\Facades\Config;
 use ItsMalikJones\BladeIconic\BladeIconicServiceProvider;
 
 class CompilesIconsTest extends TestCase
@@ -39,6 +40,34 @@ class CompilesIconsTest extends TestCase
         $expected = <<<'SVG'
         <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
         SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_default_class_from_config()
+    {
+        Config::set('blade-iconic.class', 'star');
+
+        $result = svg('iconic-star')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="star" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_merge_default_class_from_config()
+    {
+        Config::set('blade-iconic.class', 'star');
+
+        $result = svg('iconic-star', 'w-6 h-6')->toHtml();
+
+        $expected = <<<'SVG'
+                <svg class="star w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4.75L13.75 10.25H19.25L14.75 13.75L16.25 19.25L12 15.75L7.75 19.25L9.25 13.75L4.75 10.25H10.25L12 4.75Z"/></svg>
+                SVG;
 
         $this->assertSame($expected, $result);
     }
